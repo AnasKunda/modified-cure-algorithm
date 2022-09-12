3ed66d7472d50859fa6743ea797fb1a8690f2b66
Meet00732 <81899724+Meet00732@users.noreply.github.com>
2022-09-12 12:13:05 +0530

    Modified_Cure
    Modified_Cure for time series data

--- modified_dtw.py
+++ modified_dtw.py
@@ -1,7 +1,18 @@
 from dtw import *
 
-def compute_dtw(self, X, Y):
+def compute_dtw(X, Y):
     alignment = dtw(x=X, y=Y, dist_method="euclidean", keep_internals=True)
     d = alignment.distance.item()
     del alignment
     return d
+
+def compute_dtw_series(X, Y):
+    alignment = dtw(x=X, y=Y, dist_method="euclidean", keep_internals=True)
+    index1 = alignment.index1
+    index2 = alignment.index2
+    time_series = []
+    for i in range(len(index1)):
+        ans = int(X[index1[i]] - Y[index2[i]])
+        time_series.append(ans)
+
+    return time_series
